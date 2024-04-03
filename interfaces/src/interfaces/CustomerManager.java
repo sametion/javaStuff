99ef4c959aeb12dcf3ce5c375fced4067a46a230
samethion <sametacikk@gmail.com>
2024-04-03 21:58:26 +0300

    commit 4
    interfaces added

---
+++ interfaces/src/interfaces/CustomerManager.java
@@ -0,0 +1,11 @@
+package interfaces;
+
+public class CustomerManager {
+	public void add(Customer customer) {
+		System.out.println("eklendi");
+	}
+	public void delete(Customer customer) {
+		
+	}
+
+}

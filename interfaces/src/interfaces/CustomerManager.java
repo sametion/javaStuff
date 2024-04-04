1f0510c570b8eca5af9e91d7603134fbea4f7ffa
samethion <sametacikk@gmail.com>
2024-04-04 17:31:21 +0300

    commit 5
    dependency injection test

--- interfaces/src/interfaces/CustomerManager.java
+++ interfaces/src/interfaces/CustomerManager.java
@@ -1,11 +1,28 @@
 package interfaces;
 
 public class CustomerManager {
-	public void add(Customer customer) {
-		System.out.println("eklendi");
+	
+	private Logger[] loggers;
+	
+	
+	public CustomerManager(Logger[] loggers) {
+		
+		this.loggers = loggers;
 	}
+	public void add(Customer customer) {
+		
+		System.out.println("customer eklendi "+customer.getFirstName());
+
+		Utils.runLoggers(loggers, customer.getFirstName());
+		}
+	
 	public void delete(Customer customer) {
+		
+		System.out.println("customer silindi "+customer.getFirstName());
+
+		Utils.runLoggers(loggers, customer.getFirstName());
+		}
 		
 	}
 
-}
+
--- interfaces/src/interfaces/Main.java
+++ interfaces/src/interfaces/Main.java
@@ -4,6 +4,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		Logger[] loggers= {new SmsLogger(),new FileLogger(),new EmailLogger()};
+		
+		Customer customer1=new Customer(582,"samet","açık");
+		
+		CustomerManager customerManager=new CustomerManager(loggers);
+		
+		
+		customerManager.add(customer1);
+		
+		
 
 	}
 
